9788116a1d9bf820d370e4a57c528e120b088c3a
viratde <virat7107029@gmail.com>
2024-01-10 13:39:10 +0530

    Third Stage Completed

--- Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
@@ -1,5 +1,7 @@
 package tictactoe
 
+import kotlin.math.abs
+
 fun main() {
     // write your code here
 
@@ -7,9 +9,9 @@ fun main() {
     val stateString = readln()
 
     val gameState = mutableListOf(
-        stateString.slice(0..2).split("").toMutableList(),
-        stateString.slice(3..5).split("").toMutableList(),
-        stateString.slice(6..8).split("").toMutableList()
+        stateString.slice(0..2).toMutableList(),
+        stateString.slice(3..5).toMutableList(),
+        stateString.slice(6..8).toMutableList()
     )
 
     logGameState(gameState = gameState)
@@ -17,13 +19,97 @@ fun main() {
 }
 
 fun logGameState(
-    gameState: MutableList<MutableList<String>>
+    gameState: MutableList<MutableList<Char>>
 ) {
 
     println("---------")
     gameState.forEach {
-        println(it.joinToString(separator = " ", prefix = "|", postfix = "|"))
+        println(it.joinToString(separator = " ", prefix = "| ", postfix = " |"))
     }
     println("---------")
+
+    println(findGameStatus(gameState))
+}
+
+fun findGameStatus(
+    gameState: MutableList<MutableList<Char>>
+): String {
+
+    val xChars = gameState.flatten().count {
+        it == 'X'
+    }
+    val oChars = gameState.flatten().count {
+        it == 'O'
+    }
+    val empty = gameState.flatten().count {
+        it != 'X' && it != 'O'
+    }
+    if (abs(xChars - oChars) > 1) {
+        return "Impossible"
+    }
+
+    val xWins = findWinStatus(gameState, 'X')
+    val oWins = findWinStatus(gameState, 'O')
+
+    return if(xWins == 0 && oWins == 0 && empty != 0){
+        "Game not finished"
+    }else if(xWins == 0 && oWins == 0){
+        "Draw"
+    }else if(xWins > 1 || oWins > 1){
+        "Impossible"
+    }else if(xWins == 1 && oWins == 1){
+        "Impossible"
+    }else if(xWins == 1){
+        "X wins"
+    }else{
+        "O wins"
+    }
+
+}
+
+fun findWinStatus(
+    gameState: MutableList<MutableList<Char>>,
+    winnerFlag: Char
+): Int {
+
+    var wins = 0
+
+    for (i in 0..2) {
+        if (
+            gameState[i][0] == gameState[i][1] &&
+            gameState[i][1] == gameState[i][2] &&
+            gameState[i][2] == winnerFlag
+        ) {
+            wins++
+        }
+    }
+
+    for (i in 0..2) {
+        if (
+            gameState[0][i] == gameState[1][i] &&
+            gameState[1][i] == gameState[2][i] &&
+            gameState[2][i] == winnerFlag
+        ) {
+            wins++
+        }
+    }
+
+    if (
+        gameState[0][0] == gameState[1][1] &&
+        gameState[1][1] == gameState[2][2] &&
+        gameState[2][2] == winnerFlag
+    ) {
+        wins++
+    }
+
+    if (
+        gameState[0][2] == gameState[1][1] &&
+        gameState[1][1] == gameState[2][0] &&
+        gameState[2][0] == winnerFlag
+    ) {
+        wins++
+    }
+
+    return wins
 
 }
